0c41be61eb19cbb076b344dcddc1b31953a6af5d
Cog-Animesh <Animesh.Verma@cognizant.com>
2021-09-06 15:48:17 +0530

    hi

---
+++ src/main/java/Threading/Deadlock/MainDeadLock.java
@@ -0,0 +1,90 @@
+package Threading.Deadlock;
+
+import java.util.Random;
+
+public class MainDeadLock {
+
+    public static class TrainA implements Runnable{
+
+        private Intersection intersection;
+        private Random random = new Random();
+
+        public TrainA(Intersection intersection){
+            this.intersection = intersection;
+        }
+
+
+        @Override
+        public void run() {
+            while (true){
+                long sleepTime = random.nextInt(5);
+                try {
+                    Thread.sleep(sleepTime);
+                }catch (Exception r){}
+                intersection.takeRoadA();
+            }
+        }
+    }
+
+    public static class TrainB implements Runnable{
+
+        private Intersection intersection;
+        private Random random = new Random();
+
+        public TrainB(Intersection intersection){
+            this.intersection = intersection;
+        }
+
+
+        @Override
+        public void run() {
+            while (true){
+                long sleepTime = random.nextInt(5);
+                try {
+                    Thread.sleep(sleepTime);
+                }catch (Exception r){}
+                intersection.takeRoadB();
+            }
+        }
+    }
+
+    public static class Intersection{
+        private Object roadA = new Object();
+        private Object roadB = new Object();
+
+        public void takeRoadA(){
+            synchronized (roadA){
+                System.out.println("RoadA is Locked by "+Thread.currentThread().getName());
+                synchronized (roadB){
+                    System.out.println("Train is passsing through road A");
+                    try {
+                        Thread.sleep(1);
+                    }catch (Exception e){
+
+                    }
+                }
+            }
+        }
+
+        public void takeRoadB(){
+            synchronized (roadA){
+                System.out.println("Road A is Locked by "+Thread.currentThread().getName());
+                synchronized (roadB){
+                    System.out.println("Train is passsing through road B");
+                    try {
+                        Thread.sleep(1);
+                    }catch (Exception e){
+
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Intersection intersection = new Intersection();
+        Thread trainA = new Thread(new TrainA(intersection));
+        Thread trainB = new Thread(new TrainB(intersection));
+        trainA.start();
+        trainB.start();
+    }
+}
